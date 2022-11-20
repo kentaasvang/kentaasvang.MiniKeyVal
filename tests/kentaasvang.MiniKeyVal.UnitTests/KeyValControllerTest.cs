4a902acc4cac9ba67612333f9f18b20dfe817563
Kent Martin Åsvang <kent@asvang.no>
2022-11-20 11:55:38 +0100

    added test-project

---
+++ tests/kentaasvang.MiniKeyVal.UnitTests/KeyValControllerTest.cs
@@ -0,0 +1,10 @@
+namespace kentaasvang.MiniKeyVal.UnitTests;
+
+public class KeyValControllerTest
+{
+  [Fact]
+  public void Test1()
+  {
+    Assert.Equal(1, 2);    
+  }
+}
---
+++ tests/test.sh
@@ -0,0 +1,1 @@
+dotnet test tests/kentaasvang.MiniKeyVal.UnitTests/
